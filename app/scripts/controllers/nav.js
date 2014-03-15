516fc4cee227165acec4539e5ea571d09516e3c8
aditbiswas1 <aditbiswas1@gmail.com>
2014-03-16 02:18:45 +0530

    initial navbar with theming

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('zenTasksApp')
+  .controller('NavCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
