9fc04766a7534aad151f7a80397489d57973623e
aditbiswas1 <aditbiswas1@gmail.com>
2014-03-16 13:22:47 +0530

    uses gravatar as profile pic

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -1,32 +1,30 @@
 'use strict';
 
 angular.module('zenTasksApp')
-  .controller('AuthCtrl', function ($scope, $location, Auth) {
-    if (Auth.signedIn()) {
+  .controller('AuthCtrl', function ($scope, $location, Auth, User) {
+    if(Auth.signedIn()){
       $location.path('/');
     }
- 	
- 	 $scope.$on('$firebaseSimpleLogin:login', function () {
+
+    
+    $scope.$on('$firebaseSimpleLogin:login', function(){
       $location.path('/');
     });
- 
-    $scope.login = function () {
-      Auth.login($scope.user).then(function () {
-        $location.path('/')
-      }, function (error) {
-        $scope.error = error.toString();
-      });
-    };
-    $scope.register = function () {
-      Auth.register($scope.user).then(function (authUser) {
-        console.log(authUser);
-        $scope.login();
-      }, function (error) {
-        $scope.error = error.toString();
-      });
-    };
 
-    $scope.logout = function () {
-	  Auth.logout();
-	};
+    $scope.login = function(){
+      Auth.login($scope.user).then(function(){
+        $location.path('/')
+      }, function(error){
+        $scope.error = error.toString();
+      });
+    };
+    $scope.register = function(){
+      Auth.register($scope.user).then(function(authUser){
+        console.log(authUser);
+        User.create(authUser, $scope.user.username);
+        $location.path('/');
+      }, function(error){
+        $scope.error = error.toString();
+      });
+    };
   });
